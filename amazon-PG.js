2b85b1c4a656875207394c6c09c361208bfb62d8
Tevin Stowers <tstowers@Tevins-MacBook-Pro-2.local>
2020-03-07 12:05:14 -0500

    added current html and js files

---
+++ amazon-PG.js
@@ -0,0 +1,11 @@
+// alert("testing");
+
+let itemName = prompt ("What is the name of the item?");
+
+let basePrice = prompt ("Whats is the base price?");
+
+let blackFriday = prompt("Is today black Friday?");
+
+// let searchEngine = prompt("Did the purchaser find the product through a search engine?")
+
+// let shoppingSite= prompt("Did the purchaser visit a comparison-shopping site")
